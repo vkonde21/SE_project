ad25e7551f5343bb2aa9b7e66f08eab8b1d8dccd
vkonde21 <kdprasad0036@gmail.com>
2021-04-14 00:26:22 +0530

    some changes

--- backend/server.js
+++ backend/server.js
@@ -13,37 +13,19 @@ connection.once('open', () => {
 })
 const app = express();
 const port = process.env.port || 5000;
-
-var handlebars = hbs.create({
-    layoutsDir:  '/Users/vaishnavikonde/Documents/SEM_VI/SE_project/Farms/src/views/layouts',
-    helpers      : {
-        section: function(name, options){
-            if(!this._sections) this._sections = {};
-            this._sections[name] = options.fn(this);
-            return null;
-        }
-    },
-    extname: 'hbs',
-    defaultLayout: 'layout'
-    });
-
-
-//console.log(handlebars)
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //to parse json data
 app.set('view engine', 'hbs');
-app.set('views', "/Users/vaishnavikonde/Documents/SEM_VI/SE_project/Farms/src/views");
+app.set('views', "../src/views");
 app.set('view options', { layout: 'layouts/layout.hbs' });
 hbs.registerHelper("section", function(name, options){
     if(!this._sections) this._sections = {};
     this._sections[name] = options.fn(this);
     return null;
 });
-hbs.layoutsDir = '/Users/vaishnavikonde/Documents/SEM_VI/SE_project/Farms/src/views/layouts';
 app.use(cookieParser());
-
-
+app.use(express.static("public"));
 const userRouter = require('./routes/users');
 app.use('/users', userRouter);
 app.use('/', require("./routes/index"));
