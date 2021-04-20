874d194625d37a635b2aca8b44f10a8b89a69458
vkonde21 <kdprasad0036@gmail.com>
2021-04-20 12:35:33 +0530

    changes in chat links

--- src/public/static/js/chat.js
+++ src/public/static/js/chat.js
@@ -6,13 +6,16 @@ const form_input = form.querySelector('input');
 const messages = document.querySelector('#messages');
 const messageTemplate = document.getElementById('message-template').innerHTML;
 const rendermsg = Handlebars.compile(messageTemplate);
+
+//Options
+//const {username, room} = qs.parse(location.search, {ignoreQueryPrefix:true}); //ignore question mark at the start
 /*receive an event
 socket.on('event_name', () => { //function contains arguments passed by the server side
     console.log("client received the msg!!");
 });*/
 socket.on('message', (message) => {
-    console.log(rendermsg({message}));
-    messages.insertAdjacentHTML('beforeend', rendermsg({message}))
+    //console.log(rendermsg({message:message.text, createdAt: moment(message.createdAt).format('h:mm a')}));
+    messages.insertAdjacentHTML('beforeend', rendermsg({message:message.text, createdAt: moment(message.createdAt).format('D MMM h:mm a')}));
 })
 
 document.querySelector('#chatform').addEventListener('submit', (e) => {
