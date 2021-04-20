221bffd3ea2226b2f529b4f4f1f1d984663b67ad
vkonde21 <kdprasad0036@gmail.com>
2021-04-20 17:23:12 +0530

    added chatlist feature

--- backend/models/connection.model.js
+++ backend/models/connection.model.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { ObjectId } = require('bson');
+
+
 const Schema = mongoose.Schema;
 const connectionSchema = new Schema({
-    curr_user:{
-        type:ObjectId
+    chat_initiator:{
+        type:String   //username
     },
     conn_user:{
-        type:ObjectId
-    },
-    conn_user_name:{
-        type:String, //fullname of other user
+        type:String
     }
+}, 
+{
+    timestamps:true
 });
 
+
+
+connectionSchema.statics.initiateChat = async function (
+	conn_user, chat_initiator
+) {
+  try {
+    var availableRoom = await this.findOne({
+      conn_user, chat_initiator
+    });
+    if(!availableRoom){
+      availableRoom = await this.findOne({
+        conn_user:chat_initiator, chat_initiator:conn_user
+      });
+    }
+    if (!availableRoom) {
+      const newRoom = await this.create({ conn_user, chat_initiator })
+      return {
+        isNew: true,
+        message: 'creating new chat room',
+      };
+    }
+    return {
+      isNew: false,
+      message: 'old chatroom'};
+  } catch (error) {
+    console.log('error on start chat method', error);
+    throw error;
+  }
+}
 const Connection = mongoose.model('Connection', connectionSchema);
 module.exports = Connection;
