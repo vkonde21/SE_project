31a894595605af7e891c1a0564854c31a5ceb627
vkonde21 <kdprasad0036@gmail.com>
2021-04-20 09:48:11 +0530

    minor change

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -5,7 +5,7 @@ router.route('/').get(auth, async(req, res) => {
     res.render('chat_list');
 });
 router.route('/:id').get(auth, async(req, res) => {
-    res.render('chat_app');
+    res.render('chat');
 });
 //for /chat i.e / display a table of initiated chats
 module.exports = router;
