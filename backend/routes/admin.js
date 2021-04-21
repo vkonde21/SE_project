cc0b3cc77a1485c6e527dc4e36f1107ca3652214
Arya Gandhi <aryagandhi1@gmail.com>
2021-04-21 23:50:44 +0530

    admin frontend

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,4 +3,7 @@ const auth = require("../middleware/auth");
 router.route('/dashboard').get( async(req,res) => {
     res.render('admin_dashboard');
 });
+router.route('/verify_profile').get((req, res) => {
+    res.render('verify_profile');
+});
 module.exports = router;
